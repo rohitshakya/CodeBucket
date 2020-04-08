f25cfc4fd4c91731b087df89f951cfe102f6a3b6
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-04-08 17:54:29 +0530

    Update Assignment of a printf function in if else.cpp

--- Assignment of a printf function in if else.cpp
+++ Assignment of a printf function in if else.cpp
@@ -15,4 +15,4 @@ C)matter
 D)No Output
 Solution is:It doesn't matter, because if printf assigns to a char then the value of char is the size of the string which printf contain,
 and here the size of printf is null or zero therefore if statement got 0 so the else part will executed here. 
-Note: Also if execute on every non zero value*/
+Note: "if" statement execute on every non zero value*/
