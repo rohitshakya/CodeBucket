4229e7c808268d607efa8e609292b26956b991c6
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-04-14 17:15:27 +0530

    missing number xor

--- Placements/missing number.cpp
+++ Placements/missing number.cpp
@@ -1,22 +1,22 @@
 /* Missing number
-n=7, 
 	1 5 4 6 7 3
 	missing number
-	*/
+	use bitwise xor operator
+	0^x=x
+	x^x'=1
+	1^x=x' missing number	
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
 	int n=7;
-	int a[]={1,5,4,6,7,3};
-	int sum=(n*(n+1))/2;
-	cout<<sum;
-	
+	int a[]={1,5,4,6,2,3};
+	int v=0;
 	for(int i=0;i<n-1;i++)
 	{
-		sum=sum-a[i];
-		
+		v=v^a[i];		
 	}
-	cout<<"missing number is "<<sum;
+	cout<<"missing number is "<<v;
 	
 }
