1b3601702229c6148206f1a895e0d42b573a0ce2
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-04-17 16:50:23 +0530

    Travel cost and platform needed

---
+++ Placements/Minimum possible travel cost.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author : Rohit Shakya
+ * Date   : April-2020
+ *
+ * Compiler : g++ 5.1.0
+ * flags    : -std=c++14
+ */
+//Minimum possible travel cost among N cities
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[]={4, 7, 8, 3, 4 };
+	int n=sizeof(a)/sizeof(a[0]);
+	int current_cost=a[0];
+	int sum=a[0];
+	for(int i=1;i<n;i++)
+	{
+		if(a[i]<current_cost)
+		{
+			current_cost=a[i];
+		}
+		sum=sum+current_cost;
+	
+	}
+	cout<<"Total cost"<<sum<<endl;
+}
---
+++ Placements/Platform_needed.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author : Rohit Shakya
+ * Date   : April-2020
+ *
+ * Compiler : g++ 5.1.0
+ * flags    : -std=c++14
+ */
+//minimum number of platforms
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+	int a[]={900, 940, 950, 1100, 1500, 1800}; 
+    int b[] = {910, 1200, 1120, 1130, 1900, 2000}; 
+	int n=sizeof(a)/sizeof(a[0]);
+	sort(a,a+n); //O(nlogn)
+	sort(b,b+n);
+	int plat=1, res=1;
+	int i=1,j=0;
+	while(i<n&&j<n)
+	{
+		if(a[i]<b[j])
+		{
+			plat++; //arrival is less than departure
+			i++;
+			if(plat>res)
+			{
+				res=plat;
+			}
+		}
+		else
+		{
+			plat--; //departure is less arrival
+			j++;	
+		}
+		
+	}
+	cout<<res;
+	
+}
