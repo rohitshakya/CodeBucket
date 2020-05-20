365fa789a824f7995ce2ec01a2eacfb9ca899920
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-05-20 15:37:06 +0530

    Create 38. Weird algorithm.cpp

---
+++ Placements/38. Weird algorithm.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author : Rohit Shakya
+ * Date   : May-2020
+ * Compiler : g++ 4.9.2
+ * Flags    : -std=c++14
+ * Title :  weird algorithm
+ */
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int n;
+	cin>>n;
+	cout<<"\n";
+	while(n!=1)
+	{
+		cout<<n<<" ";
+		if(n&1) n=3*n+1;
+		else n>>=1;
+	}
+	cout<<n<<"\n";
+}
