c30eabfecd9b7f29a69e5fd708ba8f795020763e
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-05-29 13:12:22 +0530

    2 Pyramid pattern

---
+++ bscphp_practical/pattern3b.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5;
+	int p=1;
+	int q=1;
+	int m;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=n;j>i;j--)
+		{
+			cout<<" ";
+		}
+		m=p;
+		for(int k=0;k<=i;k++)
+		{
+			cout<<"*";
+			p--;
+			
+		}
+		for(int l=1;l<=i;l++)
+		{
+			cout<<"*";
+			p--;
+		}
+		p=m+2;
+		
+		
+	n=5;
+	p=1;
+	q=1;
+	
+		for(int j=n;j>i;j--)
+		{
+			cout<<" ";
+		}
+		for(int j=n;j>i;j--)
+		{
+			cout<<" ";
+		}
+		m=p;
+		for(int k=0;k<=i;k++)
+		{
+			cout<<"*";
+			p--;
+			
+		}
+		for(int l=1;l<=i;l++)
+		{
+			cout<<"*";
+			p--;
+		}
+		p=m+2;
+		cout<<endl;
+	}
+}
