02bbf0c78236f2968ad61dccb7c907deac252156
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-06-04 11:31:23 +0530

    String

--- Placements/String/nest.cpp
+++
@@ -1,8 +0,0 @@
-int wordNest(std::string word, std::string nest) {
-	int i=nest.length();
-	int j=word.length();
-	i=i-j;
-	int depth=i/j;
-	return depth;
-	
-}
--- Placements/String/sortstring.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	string s ="rohitmncjn";
-	sort(s.begin(),s.end());
-	//sort(s.begin(),s.end(),greater<int>());
-	cout<<s;
-}
