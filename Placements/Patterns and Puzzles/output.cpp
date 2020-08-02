77cc9eec5ed054d3a901de13c0b308fbf929bedb
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-08-02 13:15:41 +0530

    Add Misc

---
+++ Placements/Patterns and Puzzles/output.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author : Rohit Shakya
+ * Date   : July-2020
+ * Compiler : g++ 4.9.2
+ * Flags    : -std=c++14
+ * Time complexity : O(n)
+ * Title :
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	for(i=0;i<10;i++)
+	{
+		int i=10;
+		cout<<i;
+	}
+}
