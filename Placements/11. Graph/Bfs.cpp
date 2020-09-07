b456af2542e936d634db1bb0e19ce7225d8180e5
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-09-07 15:38:56 +0530

    Update

--- Placements/11. Graph/Bfs.cpp
+++ Placements/11. Graph/Bfs.cpp
@@ -6,16 +6,19 @@
  * Time complexity :
  * Title :
  */
-#include<iostream> 
-#include <list> 
+#include<bits/stdc++.h> 
 using namespace std; 
 class Graph 
 { 
 	int V;  
 	list<int> *adj; 
 public: 
-	Graph(int V); 
+	Graph(int V); // Constructor 
+
+	// function to add an edge to graph 
 	void addEdge(int v, int w); 
+
+	// prints BFS traversal from a given source s 
 	void BFS(int s); 
 }; 
 
@@ -27,24 +30,37 @@ Graph::Graph(int V)
 
 void Graph::addEdge(int v, int w) 
 { 
-	adj[v].push_back(w); 
+	adj[v].push_back(w); // Add w to v’s list. 
 } 
 
 void Graph::BFS(int s) 
 { 
+	// Mark all the vertices as not visited 
 	bool *visited = new bool[V]; 
 	for(int i = 0; i < V; i++) 
-		visited[i] = false;  
+		visited[i] = false; 
+
+	// Create a queue for BFS 
 	list<int> queue; 
+
+	// Mark the current node as visited and enqueue it 
 	visited[s] = true; 
-	queue.push_back(s);  
+	queue.push_back(s); 
+
+	// 'i' will be used to get all adjacent 
+	// vertices of a vertex 
 	list<int>::iterator i; 
 
 	while(!queue.empty()) 
 	{ 
+		// Dequeue a vertex from queue and print it 
 		s = queue.front(); 
 		cout << s << " "; 
 		queue.pop_front(); 
+
+		// Get all adjacent vertices of the dequeued 
+		// vertex s. If a adjacent has not been visited, 
+		// then mark it visited and enqueue it 
 		for (i = adj[s].begin(); i != adj[s].end(); ++i) 
 		{ 
 			if (!visited[*i]) 
@@ -65,6 +81,7 @@ int main()
 	g.addEdge(2, 0); 
 	g.addEdge(2, 3); 
 	g.addEdge(3, 3); 
+
 	cout << "Following is Breadth First Traversal "<< "(starting from vertex 2) \n"; 
 	g.BFS(2); 
 	return 0; 
