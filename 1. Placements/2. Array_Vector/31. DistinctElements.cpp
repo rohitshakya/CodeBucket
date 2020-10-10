79e75f93cd5d4352a6731a55d8c6c81f64070032
Rohit Shakya <rohit.rkshakya@gmail.com>
2020-10-10 14:26:46 +0530

    update

---
+++ 1. Placements/2. Array_Vector/31. DistinctElements.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author : Rohit Shakya
+ * Date   : July-2020
+ * Compiler : g++ 4.9.2
+ * Flags    : -std=c++14
+ * Time complexity : 
+ * Title : 
+ */
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>arr[i];
+		}
+		unordered_map<int,int> m;
+		for(int i=0;i<n;i++)
+		{
+			m[arr[i]]++;
+		}
+		int count=0;
+		for(auto i=m.begin();i!=m.end();i++)
+		{
+			if ((*i).second > 1) 
+            count += (*i).second-1;
+		}
+		cout<<count<<endl;
+	}
+}
